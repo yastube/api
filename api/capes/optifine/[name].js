909be37dfc04f1f8fa6886c34a63c2f7ccb35ccc
yastube <114358120+yastube@users.noreply.github.com>
2025-06-26 12:36:00 +0300

    Update [name].js

--- api/capes/optifine/[name].js
+++ api/capes/optifine/[name].js
@@ -1,1 +1,8 @@
-
+export default async function handler(req, res) {
+  const { user } = req.query;
+  const profileRes = await fetch(`https://optifine/capes/${user}`);
+  const buffer = await profileRes.arrayBuffer();
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Content-Type", "image/png");
+  res.status(200).send(Buffer.from(buffer));
+}
