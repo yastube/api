f2316e5da5739fcc18b88684b15dd0d3cc3000c2
yastube <114358120+yastube@users.noreply.github.com>
2025-06-26 09:33:41 +0300

    Update [uuid].js

--- api/avatar/[uuid].js
+++ api/avatar/[uuid].js
@@ -1,1 +1,9 @@
 
+export default async function handler(req, res) {
+  const { uuid } = req.query;
+  const imageUrl = `https://crafatar.com/avatars/${uuid}?size=32`;
+  const imageRes = await fetch(imageUrl);
+  const buffer = await imageRes.arrayBuffer();
+  res.setHeader("Content-Type", "image/png");
+  res.status(200).send(Buffer.from(buffer));
+}
