e15de9d12a596f0a07ab79d456e3f66fb6725e9d
yastube <114358120+yastube@users.noreply.github.com>
2025-06-26 08:09:16 +0300

    Update [uuid].js

--- api/skin/[uuid].js
+++ api/skin/[uuid].js
@@ -1,1 +1,15 @@
+export default async function handler(req, res) {
+  const { uuid } = req.query;
 
+  if (!uuid) {
+    return res.status(400).json({ error: "UUID is required" });
+  }
+
+  const imageUrl = `https://crafatar.com/avatars/${uuid}?size=64`;
+
+  const imageRes = await fetch(imageUrl);
+  const buffer = await imageRes.arrayBuffer();
+
+  res.setHeader("Content-Type", "image/png");
+  res.status(200).send(Buffer.from(buffer));
+}
