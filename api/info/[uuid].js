eafecbdf88019a1136819afabbdbea5c0758b978
yastube <114358120+yastube@users.noreply.github.com>
2025-06-26 10:03:42 +0300

    Update [uuid].js

--- api/info/[uuid].js
+++ api/info/[uuid].js
@@ -5,6 +5,12 @@ const knownUsers = {
 };
 
 export default function handler(req, res) {
+  res.setHeader("Access-Control-Allow-Origin", "*"); // or your domain
+  res.setHeader("Access-Control-Allow-Methods", "GET, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  if (req.method === "OPTIONS") {
+    return res.status(200).end();
+  }
   const { uuid } = req.query;
 
   const user = knownUsers[uuid];
