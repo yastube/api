23bde1d0c400300268b86bab8879bf5429a7feb1
yastube <114358120+yastube@users.noreply.github.com>
2025-06-26 08:31:30 +0300

    Update [uuid].js

--- api/capes/[uuid].js
+++ api/capes/[uuid].js
@@ -1,1 +1,12 @@
 
+export default async function handler(req, res) {
+  const { uuid } = req.query;
+  if (!uuid) {
+    return res.status(400).json({ error: "UUID is required" });
+  }
+  const imageUrl = `https://crafatar.com/capes/${uuid}`;
+  const imageRes = await fetch(imageUrl);
+  const buffer = await imageRes.arrayBuffer();
+  res.setHeader("Content-Type", "image/png");
+  res.status(200).send(Buffer.from(buffer));
+}
