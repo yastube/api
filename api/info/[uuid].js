78b0173309617f1e13dc18317a848f8d2d77ccb0
yastube <114358120+yastube@users.noreply.github.com>
2025-06-26 11:31:12 +0300

    Update [uuid].js

--- api/info/[uuid].js
+++ api/info/[uuid].js
@@ -1,22 +1,9 @@
-const knownUsers = {
-  "bb28c5cbccab473fa93023f81d58d63d": "Minecraft Youtuber",
-  "a-b-c-d-e": "Admin User",
-  "x-y-z": "Cool member"
-};
+import knownUsers from "./known.json"; // dot-slash = same folder
 
 export default function handler(req, res) {
   const { uuid } = req.query;
-  res.setHeader("Access-Control-Allow-Origin", "*"); // or your domain
-  res.setHeader("Access-Control-Allow-Methods", "GET, OPTIONS");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-  if (req.method === "OPTIONS") {
-    return res.status(200).end();
-  }
-  const user = knownUsers[uuid];
+  const description = knownUsers[uuid] || "Unknown User";
 
-  if (user) {
-    return res.status(200).send(user);
-  } else {
-    return res.status(404).send("Minecraft Player");
-  }
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.status(200).json({ uuid, description });
 }
