4324703e1e6bc2ddf567c0fbd5c4695b8d585130
yastube <114358120+yastube@users.noreply.github.com>
2025-06-26 08:05:37 +0300

    Update and rename test to img.js

---
+++ api/img.js
@@ -0,0 +1,11 @@
+
+export default async function handler(req, res) {
+  const imageUrl = "https://crafatar.com/avatars/069a79f4-44e9-4726-a5be-fca90e38aaf5?size=64";
+
+  const imageRes = await fetch(imageUrl);
+  const buffer = await imageRes.arrayBuffer();
+
+  res.setHeader("Content-Type", "image/png");
+  res.setHeader("Cache-Control", "public, max-age=86400");
+  res.status(200).send(Buffer.from(buffer));
+}
