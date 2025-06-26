b67199b08af0a753374a1465088b00618a238b27
yastube <114358120+yastube@users.noreply.github.com>
2025-06-26 09:56:54 +0300

    Update [uuid].js

--- api/skin/[uuid].js
+++ api/skin/[uuid].js
@@ -1,6 +1,12 @@
 export default async function handler(req, res) {
   const { uuid } = req.query;
   const imageUrl = `https://crafatar.com/skins/${uuid}?size=512`;
+  res.setHeader("Access-Control-Allow-Origin", "*"); // or your domain
+  res.setHeader("Access-Control-Allow-Methods", "GET, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  if (req.method === "OPTIONS") {
+    return res.status(200).end();
+  }
   const imageRes = await fetch(imageUrl);
   const buffer = await imageRes.arrayBuffer();
   res.setHeader("Content-Type", "image/png");
